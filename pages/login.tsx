ff93a523d01b2accfad307450420b61b637ca707
Nate Finch <nate@strattic.com>
2022-12-27 15:04:34 -0600

    add firebase auth

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,30 @@
-import React, { useEffect, useState } from 'react';
+// Import FirebaseAuth and firebase.
+import React from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth'
+
+import clientCredentials from '../firebase/clientApp';
+
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Footer from './components/footer'
+
+// Init firebase app.
+firebase.initializeApp(clientCredentials);
+
+// Configure FirebaseUI.
+const uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    // signInFlow: 'popup',
+    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    signInSuccessUrl: '/',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+};
 
 
 export default function Login() {
@@ -14,7 +37,8 @@ export default function Login() {
             </Head>
 
             <div className="Login">
-                Hi!
+                Please sign in:
+                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
             </div>
             <Footer />
         </div>
